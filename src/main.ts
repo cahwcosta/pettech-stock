dfcd0f85c68d7a2679c409ebf4f96c27d7ae4665
ana.costa <ana.costa@fiap.com.br>
2024-07-01 14:39:23 -0300

    feat(main): add github workflow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/filters/http.exception.filter';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(Number(process.env.PORT) || 3010);
 }
 bootstrap();
